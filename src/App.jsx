ed7e61c5cd8c6e882f34781cbcadbbe385843805
danchu1411 <dangctde180178@fpt.edu.vn>
2024-09-27 14:29:25 +0700

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Login from './pages/login/login.jsx'
+import Login from './pages/login/Login.jsx'
 
 function App() {
 
