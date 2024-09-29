70e8f7ffd2aa9518dc978acb1a5d542f74668707
danchu1411 <148434577+danchu1411@users.noreply.github.com>
2024-09-29 10:16:04 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <>
           <Router>
               <Routes>
-                    <Route path="/" element={<ResetPass />} />
+                    <Route path="/" element={<Login />} />
                     <Route path={"/login"} element={<Login />} />
                     <Route path={"/signup"} element={<SignUp />} />
               </Routes>
