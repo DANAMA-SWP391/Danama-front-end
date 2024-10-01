9f0d5d683de0f6051c37ad33578f7d538efb9ca9
danchu1411 <dangctde180178@fpt.edu.vn>
2024-10-01 16:11:15 +0700

    Fix redirection authentication

--- src/components/container/main-page/OptionList/OptionList.jsx
+++ src/components/container/main-page/OptionList/OptionList.jsx
@@ -5,7 +5,7 @@ function OptionList() {
     return (
         <div className="option-list">
             <div className="option">
-                <a href="/login">Films</a>
+                <a href="/.auth/login">Films</a>
             </div>
             <div className="option">
                 <a href="/signup">Cinemas</a>
