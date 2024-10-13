40ea18a2e7a2d1aa3b1e26b47232df87bd087819
danchu1411 <dangctde180178@fpt.edu.vn>
2024-10-13 14:28:19 +0700

    Add smooth scroll to OptionList.jsx

--- src/components/container/main-page/OptionList/OptionList.jsx
+++ src/components/container/main-page/OptionList/OptionList.jsx
@@ -1,16 +1,35 @@
 import "./OptionList.css";
 
 function OptionList() {
+
+    const filmListSection = document.querySelector(".film-lists");
+    const cinemaListSection = document.querySelector(".schedule-section");
+    const links = document.querySelectorAll(".option a");
+
+    for(let link of links) {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+        });
+    }
+
+    const handleFilmsClick = () => {
+        filmListSection.scrollIntoView({ behavior: "smooth" });
+    }
+
+    const handleCinemasClick = () => {
+        cinemaListSection.scrollIntoView({ behavior: "smooth" });
+    }
+
     return (
         <div className="option-list">
             <div className="option">
-                <a href="/login">Films</a>
+                <a href={""} onClick={handleFilmsClick}>Films</a>
             </div>
             <div className="option">
-                <a href="/signup">Cinemas</a>
+                <a href="" onClick={handleCinemasClick}>Cinemas</a>
             </div>
             <div className="option">
-                <a href="/reset-pass">Schedules</a>
+                <a href="" onClick={handleCinemasClick}>Schedules</a>
             </div>
         </div>
     );
