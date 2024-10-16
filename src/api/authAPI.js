d3de8d3676803bf517ef1817dd46ec93b9735949
penguink9 <giangqt2k4@gmail.com>
2024-10-16 18:55:56 +0700

    Add LoginWithGoogle

--- src/api/authAPI.js
+++ src/api/authAPI.js
@@ -1,13 +1,29 @@
 export function login(email,password) {
 
 }
-export function loginByGoogle(email) {
+export async function loginByGoogle(token) {
+    try {
+        const response = await fetch('http://localhost:8080/DANAMA_war_exploded/loginGoogle', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ idToken: token })
+        });
 
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;  // Optionally return the data for further processing
+    } catch (error) {
+        console.error("Login failed:", error);
+        throw error;  // Optionally rethrow the error for further handling
+    }
 }
+
 export function register(user) {
-
-}
-export function registerByGoogle(email) {
 
 }
 export function changePassword(user,password) {
--- src/components/container/login/GoogleSignIn/GoogleSignIn.jsx
+++ src/components/container/login/GoogleSignIn/GoogleSignIn.jsx
@@ -1,11 +1,27 @@
 import "./GoogleSignIn.css";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import {loginByGoogle} from "../../../../api/authAPI.js";
+import {UserContext} from "../../../../utils/userContext.jsx";
+import {useContext} from "react";
+import {useNavigate} from "react-router-dom";
 
 
 function GoogleSignIn() {
-    const handleSuccess = (response) => {
-        console.log("Google Sign-In successful. Token:", response);
-    }
+    const { setUser } = useContext(UserContext);
+    const navigate = useNavigate();
+    const handleSuccess = async (response) => {
+        try {
+            const token = response.credential;
+            // Call the loginByGoogle function with the token
+            const data = await loginByGoogle(token);
+            setUser(data);
+            navigate('/');
+            // Process the login success data as needed
+            console.log("User data received after login:", data);
+        } catch (error) {
+            console.error("Error during Google login:", error);
+        }
+    };
 
     const handleError = (error) => {
         console.log("Google Sign-In failed. Error:", error);
